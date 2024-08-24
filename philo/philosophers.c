ac6e0e5e57b3717002daf6916f1bfb8be1af4e4d
LegendPhoenix66 <64265723+LegendPhoenix66@users.noreply.github.com>
2024-08-24 17:41:34 +0200

    Refactor ft_atoi to handle whitespace and sign
    Enhanced the ft_atoi function to correctly process leading whitespace and optional sign. This improves robustness and correctness when converting strings to integers. Added checks for various whitespace characters and handled both positive and negative signs.
    
    Took 9 minutes

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -42,12 +42,34 @@ void *philosopher_thread(void *arg) {
 	return NULL;
 }
 
-int ft_atoi(const char *str) {
-	int result = 0;
-	for (int i = 0; str[i] != '\0'; i++) {
-		result = result * 10 + str[i] - '0';
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	{
+		i++;
 	}
-	return result;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
 
 int main(int argc, char *argv[]) {
