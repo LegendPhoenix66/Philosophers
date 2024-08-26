be2e2ec5cc58737e10acbc06050628393bfe33fb
LegendPhoenix <louis@hopp.lu>
2024-08-26 15:04:58 +0200

    Refactor philosopher thread for better readability
    Extracted eating logic into a separate function `philosopher_eat`, improving code modularity and readability. Also added proper logging for philosopher actions and ensured a controlled loop for the number of times each philosopher must eat.

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -1,50 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhopp <lhopp@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 15:04:30 by lhopp             #+#    #+#             */
+/*   Updated: 2024/08/26 15:04:30 by lhopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
 
-void *philosopher_thread(void *arg) {
-	t_philosopher *philosopher = (t_philosopher *)arg;
-
-	while (1) {
-		printf("Philosopher %d is thinking\n", philosopher->id);
-
-		// Sleep for a random amount of time
-		usleep(1000000);
-
-		printf("Philosopher %d is hungry\n", philosopher->id);
-
-		// Try to pick up the left fork
+void	philosopher_eat(const t_philosopher *philosopher)
+{
+	print_log(philosopher, "is hungry");
+	if (philosopher->id % 2 == 0)
+	{
 		pthread_mutex_lock(&philosopher->args->forks[philosopher->id]);
-		printf("Philosopher %d picked up the left fork\n", philosopher->id);
-
-		// Try to pick up the right fork
-		pthread_mutex_lock(&philosopher->args->forks[(philosopher->id + 1) % philosopher->args->num_philosophers]);
-		printf("Philosopher %d picked up the right fork\n", philosopher->id);
-
-		printf("Philosopher %d is eating\n", philosopher->id);
-
-		// Sleep for a random amount of time
-		usleep(1000000);
-
-		// Put down the right fork
-		pthread_mutex_unlock(&philosopher->args->forks[(philosopher->id + 1) % philosopher->args->num_philosophers]);
-		printf("Philosopher %d put down the right fork\n", philosopher->id);
-
-		// Put down the left fork
-		pthread_mutex_unlock(&philosopher->args->forks[philosopher->id]);
-		printf("Philosopher %d put down the left fork\n", philosopher->id);
+		print_log(philosopher, "is picking up the left fork");
+		pthread_mutex_lock(&philosopher->args->forks[(philosopher->id + 1)
+			% philosopher->args->num_philosophers]);
+		print_log(philosopher, "is picking up the right fork");
 	}
-
-	return NULL;
+	else
+	{
+		pthread_mutex_lock(&philosopher->args->forks[(philosopher->id + 1)
+			% philosopher->args->num_philosophers]);
+		print_log(philosopher, "is picking up the right fork");
+		pthread_mutex_lock(&philosopher->args->forks[philosopher->id]);
+		print_log(philosopher, "is picking up the left fork");
+	}
+	print_log(philosopher, "is eating");
+	usleep(philosopher->args->time_to_eat * 1000);
+	pthread_mutex_unlock(&philosopher->args->forks[(philosopher->id + 1)
+		% philosopher->args->num_philosophers]);
+	print_log(philosopher, "is putting down the right fork");
+	pthread_mutex_unlock(&philosopher->args->forks[philosopher->id]);
+	print_log(philosopher, "is putting down the left fork");
 }
 
-void start_philosophers(t_args * args) {
-	// Create the threads
-	pthread_t threads[args->num_philosophers];
-	for (int i = 0; i < args->num_philosophers; i++) {
-		pthread_create(&threads[i], NULL, philosopher_thread, &args->philosophers[i]);
-	}
+void	*philosopher_thread(void *arg)
+{
+	t_philosopher	*philosopher;
+	int				i;
 
-	// Wait for the threads to finish
-	for (int i = 0; i < args->num_philosophers; i++) {
+	philosopher = (t_philosopher *)arg;
+	i = 0;
+	while (philosopher->args->num_times_each_philosopher_must_eat > i++)
+	{
+		print_log(philosopher, "is thinking");
+		philosopher_eat(philosopher);
+		print_log(philosopher, "is sleeping");
+		usleep(philosopher->args->time_to_sleep * 1000);
+	}
+	return (NULL);
+}
+
+void	start_philosophers(t_args *args)
+{
+	pthread_t	threads[args->num_philosophers];
+	int			i;
+
+	i = 0;
+	while (i < args->num_philosophers)
+	{
+		pthread_create(&threads[i], NULL, philosopher_thread,
+			&args->philosophers[i]);
+		i++;
+	}
+	i = 0;
+	while (i < args->num_philosophers)
+	{
 		pthread_join(threads[i], NULL);
+		i++;
 	}
 }
